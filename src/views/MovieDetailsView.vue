6693c76f8012fa61a4eae58abe98cb01a738779e
GabrielaSenderski <gabrielasenderski.ifc@gmail.com>
2025-11-24 11:56:59 -0300

    FEAT: css details #4

--- src/views/MovieDetailsView.vue
+++ src/views/MovieDetailsView.vue
@@ -23,50 +23,58 @@
 </script>
 
 <template>
-  <div class="main">
+  <div class="main" :style="{ background: `url(https://image.tmdb.org/t/p/original${movieStore.currentMovie.backdrop_path})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }">
     <div class="content">
-      <img
+      
+      <div class="details">
+        <img
         :src="`https://image.tmdb.org/t/p/w185${movieStore.currentMovie.poster_path}`"
         :alt="movieStore.currentMovie.title"
       />
 
-      <div class="details">
+        <div class="left"> 
         <h1>Filme: {{ movieStore.currentMovie.title }}</h1>
+        <p>{{ movieStore.currentMovie.tagline }}</p>
+        <p>{{ movieStore.currentMovie.overview }}</p>
+        <p>Orçamento: ${{ movieStore.currentMovie.budget }}</p>
+        <p>Avaliação: {{ movieStore.currentMovie.vote_average }}</p>
+        </div>
+        <div class="box">
         <button
           v-if="!watchedStore.isWatched(movieStore.currentMovie.id, 'movie')"
           @click="watchedStore.addWatched(movieStore.currentMovie, 'movie')"
         >
-          Marcar como assistido
+          <span class="fa-solid fa-eye"></span>
         </button>
         <button
           v-else
           @click="watchedStore.removeWatched(movieStore.currentMovie.id, 'movie')"
         >
-          Remover dos assistidos
+          <span class="fa-solid fa-eye-slash"></span>
         </button>
         <button
           v-if="!likedStore.isLiked(movieStore.currentMovie.id, 'movie')"
           @click="likedStore.addLiked(movieStore.currentMovie, 'movie')"
         >
-          Curtir
+          <span class="fa-regular fa-heart"></span>
         </button>
         <button
           v-else
           @click="likedStore.removeLiked(movieStore.currentMovie.id, 'movie')"
         >
-          Descurtir
+          <span class="fa-solid fa-heart"></span>
         </button>
         <button
           v-if="!wishlistStore.isInWishlist(movieStore.currentMovie.id, 'movie')"
           @click="wishlistStore.addToWishlist(movieStore.currentMovie, 'movie')"
         >
-          Adicionar à lista de desejos
+          <span class="fa-solid fa-list"></span>
         </button>
         <button
           v-else
           @click="wishlistStore.removeFromWishlist(movieStore.currentMovie.id, 'movie')"
         >
-          Remover da lista de desejos
+          <span class="fa-solid fa-list-check"></span>
         </button>
         <div>
           <p>Sua avaliação:</p>
@@ -76,10 +84,8 @@
             :itemData="movieStore.currentMovie"
           />
         </div>
-        <p>{{ movieStore.currentMovie.tagline }}</p>
-        <p>{{ movieStore.currentMovie.overview }}</p>
-        <p>Orçamento: ${{ movieStore.currentMovie.budget }}</p>
-        <p>Avaliação: {{ movieStore.currentMovie.vote_average }}</p>
+        </div>
+        
       </div>
     </div>
   </div>
@@ -101,6 +107,12 @@
 </template>
 
 <style scoped>
+  .main {
+    padding: 5rem;
+    height: 100vh;
+    color: white;
+    text-shadow: 2px 2px 4px #000000;
+  }
   .companies {
     display: flex;
     flex-direction: row;
@@ -108,4 +120,40 @@
     align-items: center;
     margin-bottom: 2rem;
   }
+  .details {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-left: 3rem;
+    height: 600px;
+  }
+  .details img {
+    border-radius: 10px;
+    box-shadow: 0 0 20px #000000;
+  }
+  .left {
+    max-width: 40%;
+    margin-left: -10rem;
+  } 
+  .left h1 {
+    font-size: 4rem;
+    font-family: julius sans one, sans-serif;
+  }
+  .box {
+    display: flex;
+    flex-direction: column;
+    row-gap: 1rem;
+    align-items: flex-start;
+    margin-top: 300px;
+    background-color: #111111;
+    padding: 2rem;
+  }
+  .box button {
+    background: none;
+    border: none;
+    color: white;
+    cursor: pointer;
+  }
+
+  
 </style>
